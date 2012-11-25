88e2cc8f6e7813d8b5a7ff56de1fa97e8878d0ae
Alfonso Roman <alroman@ucla.edu>
2012-11-24 17:42:46 -0800

    updating library

---
+++ js/decorator.js
@@ -0,0 +1,49 @@
+/* HTML rendering lib */
+
+function html_element(elem) {
+
+    this.tag = elem.tag || 'div';
+    this.content = elem.content || '';
+    this.attribs = elem.attribs || {};
+
+    // Render the contents of this tag
+    this.render = function() {
+        var out = '<';
+        out += this.tag;
+
+        var name;
+        for(name in elem.attribs) {
+            out += ' ' + name + '="' + elem.attribs[name] + '"';
+        }
+
+        out += '>';
+        out += this.content
+        out += '</' + this.tag + '>' + "\n";
+
+        // return HTML
+        return out;
+    }
+
+    this.addClass = function(name) {
+        if(this.attribs['class'] == undefined) {
+            this.attribs['class'] = name;
+
+        } else {
+            this.attribs['class'] += ' ' + name;
+
+        }
+    }
+}
+
+var f = new html_element({
+    tag : 'div',
+    content : 'foo',
+    attribs : {
+        'id' : 'gargar'
+    }
+})
+
+f.addClass('classa');
+
+console.log('right here');
+console.log(f.render());
